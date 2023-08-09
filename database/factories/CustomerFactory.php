40cd1cb7dda90ba4557c53f361192bf906deb4b8
yousra.essam@jumia.com <yousra.essam@jumia.com>
2023-08-10 00:54:15 +0300

    remove commented code

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -3,8 +3,6 @@
 namespace Database\Factories;
 
 use App\Enums\GenderType;
-use App\Models\Customer;
-use App\Models\Group;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -32,17 +30,4 @@ class CustomerFactory extends Factory
             'birth_date' => $this->faker->date('Y-m-d', 'now')
         ];
     }
-
-    // /**
-    //  * Configure the model factory.
-    //  */
-    // public function configure(): static
-    // {
-    //     $groups = Group::all();
-    //     return $this->afterCreating(function (Customer $customer) use ($groups){
-    //         // $customer->groups()->attach(rand(1, 3));
-    //         $customer->groups()->saveMany($groups);
-
-    //     });
-    // }
 }
