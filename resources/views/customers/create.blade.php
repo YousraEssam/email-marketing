c9a0bcfbd0ec178a49c929396299db954aa41982
yousra.essam@jumia.com <yousra.essam@jumia.com>
2023-08-10 20:26:44 +0300

    fix title

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -2,7 +2,7 @@
     <x-flatpickr::style />
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Create new customer') }}
         </h2>
     </x-slot>
     <div class="py-12">
